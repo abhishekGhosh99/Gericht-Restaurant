cc4438b1078aa835a20438f6745b7457f3a31780
abhishekGhosh99 <125865872+abhishekGhosh99@users.noreply.github.com>
2023-10-05 14:53:42 +0530

    commit

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,41 +1,40 @@
-import React, { useRef, useState } from "react";
-import "./Intro.css";
-// import { BsFillPlayFill, BsPause, BsPauseFill } from "react-icons/bs";
+import React from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+
 import { meal } from "../../constants";
+import "./Intro.css";
 
 const Intro = () => {
-  const [playVideo, setPlayVideo] = useState(false);
-  const vidRef = useRef();
-  const handleVideo = () => {
-    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
-    if (playVideo) {
-      vidRef.current.pause();
-    } else {
-      vidRef.current.play();
-    }
-  };
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
 
   return (
     <div className="app__video">
       <video
-        src={meal}
         ref={vidRef}
+        src={meal}
         type="video/mp4"
         loop
-        autoPlay
         controls={false}
         muted
       />
-      <div className="app__video-overlay flex__center ">
+      <div className="app__video-overlay flex__center">
         <div
           className="app__video-overlay_circle flex__center"
-          onClick={handleVideo}
+          onClick={() => {
+            setPlayVideo(!playVideo);
+            if (playVideo) {
+              vidRef.current.pause();
+            } else {
+              vidRef.current.play();
+            }
+          }}
         >
-          {/* {playVideo ? (
+          {playVideo ? (
             <BsPauseFill color="#fff" fontSize={30} />
           ) : (
             <BsFillPlayFill color="#fff" fontSize={30} />
-          )} */}
+          )}
         </div>
       </div>
     </div>
